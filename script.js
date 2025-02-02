0080ac3e600b339a0c64d0ed2541776fbbc951b4
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-02-02 14:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function getFormvalue(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-
-    alert(`${firstName} ${lastName}`);
+function getFormvalue() {
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    alert(firstName + " " + lastName);
 }
-
-document.getElementById('nameForm').addEventListener('submit', getFormvalue);
